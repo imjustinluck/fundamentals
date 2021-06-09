b7328d780d2fa75e06f28108f88155486e523361
imjustinluck <justinaguilo@live.com>
2021-06-08 19:49:11 -0700

    second

--- oop/modularizing/hotdog.py
+++ oop/modularizing/hotdog.py
@@ -16,6 +16,6 @@ print(user.say_hello())
 print(__name__)
 
 if __name__ == "__main__":
-    product = Product([args])
-    print(product)
-    print(product.add_tax(0.18))
+    print("the file is being executed directly")
+else:
+    print("The file is being executed because it is imported by another file. The file is called: ", __name__)
